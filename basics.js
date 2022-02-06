acb086c411765d115fb4ab316205ea2c93110065
mathilde <math68n8@stud.kea.dk>
2022-02-06 19:37:52 +0100

    template literals done

--- basics.js
+++ basics.js
@@ -1,1 +1,11 @@
+"use strict";
 console.log("i am the javascript");
+
+
+const value = "";
+
+if (value){
+    console.log("value is truthy");
+}else{
+    console.log("value is falsy");
+}
---
+++ template.js
@@ -0,0 +1,8 @@
+const petname = "Hossein";
+const animal= "Idiot";
+const name = "Mathilde";
+
+const text = `My name is ${name}.
+I have a ${animal} named ${petname}`
+
+console.log(text);
