563ecce8c30c4ef6124ab13735ccfafd91182442
mathilde <math68n8@stud.kea.dk>
2022-02-15 13:19:31 +0100

    rgb to hex done

--- colorconvert.js
+++ colorconvert.js
@@ -1,5 +1,9 @@
 const body = document.querySelector("body");
 
+
+
+
+
 function hexToRGB (hexcolor = "#123456"){
     const rhex = hexcolor.substring(1,3);
     const ghex = hexcolor.substring(3,5);
@@ -17,10 +21,39 @@ function hexToRGB (hexcolor = "#123456"){
     };
  */
 
+    //adds all converted colors to correct format
     const RGB= R+ "," + G + "," + B;
     console.log(RGB);
+
+    //just for funsies, changed the background color of body
     body.style.backgroundColor = "rgb(" + RGB + ")";
     
 }
 
+function colorToHex(color){
+// this function converts a color from number (rgb) into hex and returns the hex ( if the lenght of hex is 1, an 0 is added in front)
+let hex = color.toString(16);
+if (hex.length == 1 ){
+    hex = "0" + hex;
+}
+return hex;
+}
+
+function rgbToHex (r,g,b){
+    //converts each color into hex using the to colortohex function.
+  const rhex= colorToHex(r);
+  const  ghex= colorToHex(g);
+  const  bhex= colorToHex(b);
+
+   //adds all the converted numbers to the correct hex format and logs it
+    const hex = `#${rhex}${ghex}${bhex}`;
+    console.log(hex);
+
+     //just for funsies, changed the background color of body
+    body.style.backgroundColor = hex;
+
+}
+
 hexToRGB("#c0ffee");
+rgbToHex (192,102,37);
+
